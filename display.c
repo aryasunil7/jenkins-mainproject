2557be16aa27b2e9c91fbfdc0004f4339c5025d2
Arya Sunil <arya.sunil7@gmail.com>
2018-08-06 21:12:49 +0530

    first commit

---
+++ display.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void mydisplay(void)
+{
+	cout << "Displaying Information: " << endl;
+
+	// Displaying information
+	for(int i = 0; i < 10; ++i)
+		{
+			cout << "\nRoll number: " << i+1 << endl;
+			cout << "Name: " << s[i].name << endl;
+		}
+	return
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+studentmake: student.c display.c
+	gcc -o studentmake student.c display.c -I.
